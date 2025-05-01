97630e629578cbf5f1a92c5651a287031a7a32f5
Kieran Barker <kieran.barker@multiverse.io>
2025-05-01 16:42:03 +0100

    Add missing async/await keywords

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const app = require("./src/app");
 const db = require("./db/connection");
+const app = require("./src/app");
+
+// TODO: Create your GET Request Route Below:
+
 const port = 3000;
 
-//TODO: Create your GET Request Route Below:
-
-app.listen(port, () => {
-  db.sync();
+app.listen(port, async () => {
+  await db.sync();
   console.log(`Listening at http://localhost:${port}/restaurants`);
 });
